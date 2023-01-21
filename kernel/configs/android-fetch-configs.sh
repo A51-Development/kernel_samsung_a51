12559182981bf8e3899f5da91ee54e9edc5927e3
roynatech2544 <whiteshell2544@naver.com>
2023-01-21 12:20:09 +0000

    kernel/configs: Modify fetch script to fetch latest
    TODO: The link will be invalid when AOSP drops 4.14 q

--- kernel/configs/android-fetch-configs.sh
+++ kernel/configs/android-fetch-configs.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-curl https://android.googlesource.com/kernel/configs/+archive/master/android-4.14.tar.gz | tar xzv
-
+CONFIG_FILE=kernel/configs/android-recommended-arm64.config
+# download_googlesource: Download a text file from android.googlesource.com and append to a file
+# param 1: The absolute https:// link
+# param 2: Destination file
+function download_googlesource() {
+	curl $1?format=TEXT | base64 --decode >> $2
+}
+rm -f $CONFIG_FILE
+download_googlesource https://android.googlesource.com/kernel/configs/+/refs/heads/master/q/android-4.14/android-base.config $CONFIG_FILE
+download_googlesource https://android.googlesource.com/kernel/configs/+/refs/heads/master/q/android-4.14/android-recommended-arm64.config $CONFIG_FILE
+download_googlesource https://android.googlesource.com/kernel/configs/+/refs/heads/master/q/android-4.14/android-recommended.config $CONFIG_FILE
